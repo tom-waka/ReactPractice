5d666996061e4030b917dc38d740e0851bb6c508
tom-waka <one.sukt.one@gmail.com>
2020-06-04 23:16:57 +0900

    コンポーネントのライフサイクルについて勉強

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
+import LikeButton from './LikeButton';
 
 const Article = (props) => {
-  let publishState = "";
-  if (props.isPublished) {
-    publishState = "公開"
-  } else {
-    publishState = "非公開"
-  }
   return (
     <div>
       <h2>{props.title}</h2>
-      <p>順番は{props.order}です</p>
+      <label htmlFor="check">公開状態</label>
+      <input type="checkbox" checked = {props.isPublished} id="check" onClick={() => props.toggle()} />
+      <LikeButton count={props.count}/>
     </div>
   )
 };
